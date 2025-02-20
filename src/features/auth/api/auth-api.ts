ee337dbcfa56eff93323afb4c5319e2941213f7e
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-02-20 19:43:02 +0300

    refactor: обновил authApi, закомментировав методы signIn и me для будущей реализации

--- src/features/auth/api/auth-api.ts
+++ src/features/auth/api/auth-api.ts
@@ -14,6 +14,14 @@ type SignUpResponse = {
 
 export const authApi = {
   signUp: async (signUpData: SignUpDataT) => {
-      return await axios.post<null, AxiosResponse<SignUpResponse>, SignUpDataT>(`${settings.API_URL}/api/v1/auth/register`, signUpData)
-  }
+    return await axios.post<null, AxiosResponse<SignUpResponse>, SignUpDataT>(`${settings.API_URL}/api/v1/auth/register`, signUpData)
+  },
+
+  // signIn: async (signInData: SignInDataT) => {
+  //   return await axios.post<null, AxiosResponse<SignInResponse>, SignInDataT>(`${settings.API_URL}/api/v1/auth/login`, signInData)
+  // }
+
+  // me: async () => {
+  //   return await axios.get(`${settings.API_URL}/api/v1/auth/me`)
+  // }
 }
