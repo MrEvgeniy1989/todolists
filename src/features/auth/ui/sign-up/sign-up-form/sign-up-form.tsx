d844bf35560dd8b4417ff81507b062d2683f502b
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-02-27 22:42:38 +0300

    feat: добавил новые компоненты формы с поддержкой react-hook-form и обновил стили

--- src/features/auth/ui/sign-up/sign-up-form/sign-up-form.tsx
+++ src/features/auth/ui/sign-up/sign-up-form/sign-up-form.tsx
@@ -2,14 +2,31 @@
 
 import { authApi, SignUpDataT } from "@/features/auth/api/auth-api";
 import { Card } from "@/shared/components/ui/card/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/shared/components/ui/form/form";
 import { Input } from "@/shared/components/ui/input/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
 
 type Props = {
   setIsOpenSuccessfulModalAction: (isOpenSuccessfulModal: boolean) => void;
 };
+
+const SignUpFormSchema = z.object({
+  login: z.string().min(4, {
+    message: "Login must be at least 4 characters.",
+  }),
+  email: z.string().email("Email is required"),
+  password: z.string().min(4, {
+    message: "Password must be at least 4 characters.",
+  }),
+  confirmPassword: z.string().min(4, {
+    message: "Password must be at least 4 characters.",
+  }),
+});
 
 export const SignUpForm = ({ setIsOpenSuccessfulModalAction }: Props) => {
   const { mutate } = useMutation({
@@ -22,53 +39,96 @@ export const SignUpForm = ({ setIsOpenSuccessfulModalAction }: Props) => {
     },
   });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SignUpDataT>();
-
-  const onFormDataSubmit = handleSubmit((formData: SignUpDataT) => {
-    mutate(formData);
+  const form = useForm<z.infer<typeof SignUpFormSchema>>({
+    resolver: zodResolver(SignUpFormSchema),
+    defaultValues: {
+      login: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   });
 
+  const onFormDataSubmit = (formData: z.infer<typeof SignUpFormSchema>) => {
+    mutate(formData);
+    toast("You submitted the following values:");
+  };
+
   return (
-    <Card className={"flex min-w-[400px] flex-col"} asChild>
-      <form onSubmit={onFormDataSubmit}>
-        <h1 className={`mb-5 text-center`}>Sign Up</h1>
-        <Input
-          className={`mb-3`}
-          placeholder="login"
-          {...register("login", { required: true })}
-        />
-        {errors.login && <span className={'text-red-400'}>This field is required</span>}
+    <Card className={"w-full max-w-105 max-sm:max-w-[335px]"}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onFormDataSubmit)} className={"flex flex-col"}>
+          <h1 className={`mb-5 text-center`}>Sign Up</h1>
 
-        <Input
-          className={`mb-3`}
-          placeholder="email"
-          {...register("email", { required: true })}
-        />
-        {errors.email && <span>This field is required</span>}
+          <FormField
+            control={form.control}
+            name="login"
+            render={({ field }) => (
+              <FormItem className={`mb-3`}>
+                <FormLabel>Login</FormLabel>
+                <FormControl>
+                  <Input type={"text"} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <Input
-          className={`mb-3`}
-          placeholder="password"
-          {...register("password", { required: true })}
-        />
-        {errors.password && <span>This field is required</span>}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className={`mb-3`}>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type={"email"} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <Input
-          className={`mb-3`}
-          placeholder="confirm password"
-          {...register("confirmPassword", { required: true })}
-        />
-        {errors.confirmPassword && <span>This field is required</span>}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className={`mb-3`}>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type={"password"} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <button type="submit" className={`mt-3 bg-primary border border-border py-1 px-10 shadow-xs shadow-shadow rounded-lg`} >Submit</button>
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem className={`mb-3`}>
+                <FormLabel>Confirm password</FormLabel>
+                <FormControl>
+                  <Input type={"password"} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <span className={'text-base text-center mt-5'}>Do you have account?</span>
-        <Link href={'/auth/sign-in'} className={'text-primary text-center'}>Sign In</Link>
-      </form>
+          <button
+            type="submit"
+            className={`bg-primary border-border shadow-shadow mt-3 rounded-lg border px-10 py-1 shadow-xs`}
+          >
+            Submit
+          </button>
+
+          <span className={"mt-5 text-center text-base"}>Do you have account?</span>
+          <Link href={"/auth/sign-in"} className={"text-primary text-center"}>
+            Sign In
+          </Link>
+        </form>
+      </Form>
     </Card>
   );
 };
