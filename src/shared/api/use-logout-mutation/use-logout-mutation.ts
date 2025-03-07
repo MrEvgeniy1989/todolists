bc71464192269184d451c41f0c7681e17a4bf5fe
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-03-08 02:36:38 +0300

    feat: добавлен хук useLogoutMutation и обновлена навигация в Header

---
+++ src/shared/api/use-logout-mutation/use-logout-mutation.ts
@@ -0,0 +1,30 @@
+"use client"
+import { authApi } from "@/features/auth/api/auth-api"
+import { useAuthStore } from "@/features/auth/model/auth-store"
+import { useMutation } from "@tanstack/react-query"
+import { AxiosError } from "axios"
+import toast from "react-hot-toast"
+import { useRouter } from "next/navigation"
+
+export const useLogoutMutation = () => {
+  const { setIsLoggedIn } = useAuthStore()
+  const router = useRouter()
+
+  const logoutMutation = useMutation({
+    mutationFn: async () => {
+      const response = await authApi.logout()
+      return response
+    },
+    onSuccess: () => {
+      localStorage.removeItem("accessToken")
+      setIsLoggedIn(false)
+      toast.success("Exit is successful")
+      router.push("/auth/sign-in")
+    },
+    onError: (error: AxiosError<{ message: string }>) => {
+      toast.error(error.response?.data?.message || "Something went wrong")
+    },
+  })
+
+  return logoutMutation
+}
--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,50 +1,34 @@
-"use client";
-import { authApi } from "@/features/auth/api/auth-api";
-import { ChangeThemeButton } from "@/shared/components/change-theme-button/change-theme-button";
-import { LogoIcon } from "@/shared/components/icons/logo-icon";
-import { useMutation } from "@tanstack/react-query";
-import { AxiosError } from "axios";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
+"use client"
+import { useAuthStore } from "@/features/auth/model/auth-store"
+import { useLogoutMutation } from "@/shared/api/use-logout-mutation/use-logout-mutation"
+import { ChangeThemeButton } from "@/shared/components/change-theme-button/change-theme-button"
+import { LogoIcon } from "@/shared/components/icons/logo-icon"
+import Link from "next/link"
 
 export const Header = () => {
-  const router = useRouter();
-  const { mutate: logout } = useMutation({
-    mutationFn: async () => {
-      console.log("Starting logout...");
-      const response = await authApi.logout();
-      console.log("Logout response:", response);
-      return response;
-    },
-    onSuccess: () => {
-      console.log("Logout successful");
-      localStorage.removeItem("accessToken");
-      router.push("/auth/sign-in");
-    },
-    onError: (error: AxiosError<{ message: string }>) => {
-      console.error("Logout error:", error);
-      toast.error(error.response?.data?.message || "Something went wrong");
-    },
-  });
+  const { isLoggedIn } = useAuthStore()
+  const { mutate: logout } = useLogoutMutation()
 
   const handleLogout = () => {
-    const cookies = document.cookie;
-    console.log("Current cookies:", cookies);
-    logout();
-  };
+    logout()
+  }
 
   return (
     <header
-      className={"h-headerHeight bgSecondary flex w-full items-center justify-between pt-1 pr-5 pb-1 pl-5 shadow-md"}
+      className={
+        "h-headerHeight bgSecondary flex w-full items-center justify-between pt-1 pr-5 pb-1 pl-5 shadow-md"
+      }
     >
       <Link href={"/"}>
         <LogoIcon />
       </Link>
       <div className={"flex gap-2"}>
-        <button onClick={handleLogout}>Logout</button>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/auth/sign-in"}>Sign In</Link>
+        <Link href={"/auth/sign-up"}>Sign up</Link>
+        {isLoggedIn && <button onClick={handleLogout}>Logout</button>}
         <ChangeThemeButton />
       </div>
     </header>
-  );
-};
+  )
+}
