c49218f47762b9e8ff231b8c5e211665e4146c75
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-03-08 02:38:29 +0300

    refactor: улучшена обработка ошибок в хуке useMeQuery

--- src/shared/api/use-me-query/use-me-query.ts
+++ src/shared/api/use-me-query/use-me-query.ts
@@ -1,12 +1,25 @@
-import { authApi } from "@/features/auth/api/auth-api";
-import { useQuery } from "@tanstack/react-query";
+import { UserT } from "@/entities/user/model/user.types"
+import { authApi } from "@/features/auth/api/auth-api"
+import { useQuery } from "@tanstack/react-query"
+import { AxiosError, AxiosResponse } from "axios"
+import { useEffect } from "react"
 
 export const useMeQuery = () => {
-  return useQuery({
+  const query = useQuery<UserT, AxiosError>({
     queryKey: ["me"],
     queryFn: async () => {
-      return await authApi.me();
+      return authApi.me().then((res: AxiosResponse<UserT>) => {
+        return res.data
+      })
     },
     retry: false,
-  });
-};
+  })
+
+  useEffect(() => {
+    if (query.error) {
+      console.log("useMeQuery: Request Error:", query.error?.response?.status)
+    }
+  }, [query.error])
+
+  return query
+}
