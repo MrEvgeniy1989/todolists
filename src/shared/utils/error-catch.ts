d01befc3bdf0b6b8397cde2a3e7f383bc3b84326
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-03-07 22:33:48 +0300

    feat: добавлена утилита обработки серверных ошибок

---
+++ src/shared/utils/error-catch.ts
@@ -0,0 +1,15 @@
+import { AxiosError } from 'axios'
+
+type ErrorResponse = {
+  message: string | string[];
+}
+
+/** обработка серверных ошибок */
+export const errorCatch = (error: AxiosError<ErrorResponse>): string =>
+  error.response && error.response.data
+    ? typeof error.response.data.message === 'object'
+      ? error.response.data.message[0]
+      : error.response.data.message
+    : error.message
+
+
