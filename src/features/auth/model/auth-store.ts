14cabbd349b74df0fb0e65a839a85fc4400c25f4
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-03-13 21:17:05 +0300

    refactor: улучшена типизация и структура хранилища аутентификации в auth-store

--- src/features/auth/model/auth-store.ts
+++ src/features/auth/model/auth-store.ts
@@ -1,16 +1,20 @@
-import { UserT } from "@/entities/user/model/user.types";
-import { create } from "zustand";
+import { UserT } from "@/entities/user/model/user.types"
+import { create } from "zustand"
 
 export type AuthStoreT = {
-  isLoggedIn: boolean;
-  user: UserT | null;
-  setIsLoggedIn: (isLoggedIn: boolean) => void;
-  setUser: (user: UserT) => void;
-};
+  isLoggedIn: boolean
+  user: UserT | null
+  setIsLoggedIn: (isLoggedIn: boolean) => void
+  setUser: (user: UserT | null) => void
+}
 
 export const useAuthStore = create<AuthStoreT>()((set) => ({
   isLoggedIn: false,
   user: null,
-  setIsLoggedIn: (isLoggedIn: boolean) => set({ isLoggedIn }),
-  setUser: (user: UserT | null) => set({ user }),
-}));
+  setIsLoggedIn: (isLoggedIn: boolean) => {
+    set({ isLoggedIn })
+  },
+  setUser: (user: UserT | null) => {
+    set({ user })
+  },
+}))
