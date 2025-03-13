29c8ea1d0bd13f5984d709bec8b0b88aca9c26f2
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-03-13 21:18:24 +0300

    refactor: обновлен компонент SignInForm с использованием нового хука useLoginMutation и улучшением локализации текстов

--- src/features/auth/ui/sign-in/sign-in-form/sign-in-form.tsx
+++ src/features/auth/ui/sign-in/sign-in-form/sign-in-form.tsx
@@ -1,62 +1,53 @@
-"use client";
+"use client"
 
-import { authApi } from "@/features/auth/api/auth-api";
-import { SignInFormSchema, SignInFormValues } from "@/features/auth/model/validators/sign-in-validation-schema";
-import { Card } from "@/shared/components/ui/card/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/shared/components/ui/form/form";
-import { Input } from "@/shared/components/ui/input/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
-import { AxiosError } from "axios";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
+import {
+  LoginFormSchema,
+  LoginFormValuesT,
+} from "@/features/auth/model/validators/sign-in-validation-schema"
+import { useLoginMutation } from "@/shared/api/use-sign-in-mutation"
+import { Card } from "@/shared/components/ui/card/card"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shared/components/ui/form/form"
+import { Input } from "@/shared/components/ui/input/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import Link from "next/link"
+import { useForm } from "react-hook-form"
 
 export const SignInForm = () => {
-  const router = useRouter();
-  const { mutate } = useMutation({
-    mutationFn: async (formData: SignInFormValues) => {
-      return authApi.signIn(formData);
-    },
-    onSuccess: (response) => {
-      if ("accessToken" in response.data) {
-        localStorage.setItem("accessToken", response.data.accessToken);
-        router.push("/todolists");
-      }
-    },
-    onError: (error: AxiosError<{ message: string }>) => {
-      toast.error(error.response?.data?.message || "Something went wrong");
-    },
-  });
+  const { mutate: signInMutate, isPending: isSignInPending } = useLoginMutation()
 
-  const form = useForm<SignInFormValues>({
-    resolver: zodResolver(SignInFormSchema),
+  const form = useForm<LoginFormValuesT>({
+    resolver: zodResolver(LoginFormSchema),
     defaultValues: {
       loginOrEmail: "",
       password: "",
     },
-  });
+  })
 
-  const onFormDataSubmit = (formData: SignInFormValues) => {
-    mutate(formData);
-  };
+  const onFormDataSubmit = (formData: LoginFormValuesT) => {
+    signInMutate(formData)
+  }
 
   return (
-    <Card className={"w-full max-w-105 max-sm:max-w-[335px]"}>
-      <Link href={"/"}>Home</Link>
+    <Card className="w-full max-w-105 p-6 max-sm:max-w-[335px]">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onFormDataSubmit)} className={"flex flex-col"}>
-          <h1 className={`mb-5 text-center`}>Sign In</h1>
+        <form onSubmit={form.handleSubmit(onFormDataSubmit)} className="flex flex-col">
+          <h1 className="mb-5 text-center text-2xl font-bold">Вход</h1>
 
           <FormField
             control={form.control}
             name="loginOrEmail"
             render={({ field }) => (
-              <FormItem className={`mb-3`}>
-                <FormLabel>Login or Email</FormLabel>
+              <FormItem className="mb-3">
+                <FormLabel>Логин или Email</FormLabel>
                 <FormControl>
-                  <Input type={"text"} {...field} />
+                  <Input type="text" {...field} disabled={isSignInPending} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -68,9 +59,9 @@ export const SignInForm = () => {
             name="password"
             render={({ field }) => (
               <FormItem className={`mb-3`}>
-                <FormLabel>Password</FormLabel>
+                <FormLabel>Пароль</FormLabel>
                 <FormControl>
-                  <Input type={"password"} {...field} />
+                  <Input type="password" {...field} disabled={isSignInPending} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -80,18 +71,22 @@ export const SignInForm = () => {
           <button
             type="submit"
             className={`bg-primary border-border shadow-shadow mt-4 rounded-lg border px-10 py-1 shadow-xs`}
+            disabled={isSignInPending}
           >
-            Submit
+            {isSignInPending ? "Выполняется вход..." : "Войти"}
           </button>
 
           <div className="mt-3 flex items-center justify-center gap-x-2 text-sm">
-            <span>You don&#39;t have an account?</span>
-            <Link href={"/auth/sign-up"} className={"text-primary-dark-400 text-center"}>
-              Sign Up
+            <span className="text-sm">Еще нет аккаунта? </span>
+            <Link
+              href="/auth/sign-up"
+              className="text-primary-dark-400 text-center text-sm hover:underline"
+            >
+              Зарегистрироваться
             </Link>
           </div>
         </form>
       </Form>
     </Card>
-  );
-};
+  )
+}
