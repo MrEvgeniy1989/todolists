37878e588a0e64be3d69d473e0440b20af05a654
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-03-13 21:17:20 +0300

    feat: добавлен хук useLoginMutation для обработки входа пользователя с уведомлениями об успехе и ошибках

---
+++ src/shared/api/use-sign-in-mutation.ts
@@ -0,0 +1,35 @@
+"use client"
+import { authApi } from "@/features/auth/api/auth-api"
+import { useAuthStore } from "@/features/auth/model/auth-store"
+import { LoginFormValuesT } from "@/features/auth/model/validators/sign-in-validation-schema"
+import { useMutation } from "@tanstack/react-query"
+import { AxiosError, AxiosResponse } from "axios"
+import { useRouter } from "next/navigation"
+import toast from "react-hot-toast"
+import { queryClient } from "./query-client"
+import { ResponseWithAccessToken } from "@/features/auth/api/auth-api.types"
+
+export const useLoginMutation = () => {
+  const { setIsLoggedIn } = useAuthStore()
+  const router = useRouter()
+
+  const loginMutation = useMutation({
+    mutationFn: async (loginData: LoginFormValuesT) => {
+      return await authApi.login(loginData)
+    },
+    onSuccess: (response: AxiosResponse<ResponseWithAccessToken>) => {
+      const { accessToken } = response.data
+      localStorage.setItem("accessToken", accessToken)
+      setIsLoggedIn(true)
+      queryClient.invalidateQueries({ queryKey: ["me"] })
+      toast.success("Вы успешно вошли в систему")
+      router.push("/todolists")
+    },
+    onError: (error: AxiosError<{ message: string }>) => {
+      toast.error(error.response?.data?.message || "Что-то пошло не так...")
+      console.error(error.response?.data?.message || "Что-то пошло не так...")
+    },
+  })
+
+  return loginMutation
+}
