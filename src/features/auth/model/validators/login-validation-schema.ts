09bd855923a2bed7e04c5b24b24392dff8bfe028
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-03-14 11:22:57 +0300

    refactor: заменена валидация пароля в схеме входа на переиспользуемую схему passwordSchema для улучшения структуры кода

--- src/features/auth/model/validators/login-validation-schema.ts
+++ src/features/auth/model/validators/login-validation-schema.ts
@@ -1,12 +1,10 @@
 import { z } from "zod"
-
+import { passwordSchema } from './password-validation-schema'
 export const LoginFormSchema = z.object({
   loginOrEmail: z.string().min(4, {
     message: "Логин или email должен быть не менее 4 символов.",
   }),
-  password: z.string().min(4, {
-    message: "Пароль должен быть не менее 4 символов.",
-  }),
+  password: passwordSchema,
 })
 
 export type LoginFormValuesT = z.infer<typeof LoginFormSchema>
